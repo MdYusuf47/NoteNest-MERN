a389b07ece60af1bc4284a51f0cadf5ceae8abff
Md Yusuf <mdyusuf742171@gmail.com>
2025-06-27 22:39:35 +0530

    add rate limiting functionality with Upstash and integrate into server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import notesRouters from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import rateLimiter from './middleware/ratelimiter.js';
 
 dotenv.config()
 const app  = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json()); // Middleware to parse JSON bodies 
+app.use(rateLimiter)
 
 app.use("/api/notes",notesRouters)
 
