a0444bc7d9b4f068c89bc194dee332a21bd6fbb1
Md Yusuf <mdyusuf742171@gmail.com>
2025-06-28 23:23:16 +0530

    add tailwind css and DaisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailsPage from './pages/NoteDetailsPage'
+import toast from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+    <button className="btn btn-accent">Accent</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailsPage />} />
+      </Routes>
+    </div>
   )
 }
 
