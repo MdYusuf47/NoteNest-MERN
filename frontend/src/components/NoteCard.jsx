007438903b4045acf2a3571f51aa65f00e49c2d7
Md Yusuf <mdyusuf742171@gmail.com>
2025-07-01 13:18:38 +0530

    add delete functionality to notes and handle empty state in home page

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,9 +1,22 @@
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
-import React from "react";
 import { Link } from "react-router";
 import { formatDate } from "../lib/utils";
+import api from "../lib/axios";
+import { toast } from "react-hot-toast";
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note,setNotes }) => {
+  const handleDelete = async (e, id) => {
+    e.preventDefault(); // get rid of the default link behavior
+    if (!window.confirm("Are you sure you want to delete this note?")) return;
+    try {
+      await api.delete(`/notes/${id}`);
+      setNotes((prev) => prev.filter(note => note._id !== id)); // Update the notes state to remove the deleted note
+      toast.success("Note deleted successfully");
+    } catch (error) {
+      console.log("Error deleting note:", error);
+      toast.error("Failed to delete note");
+    }
+  };
   return (
     <Link
       to={`/note/${note._id}`}
@@ -14,15 +27,18 @@ const NoteCard = ({ note }) => {
         <h3 className="card-title text-base-content">{note.title}</h3>
         <p className="text-base-content/70 line-clamp-3">{note.content}</p>
         <div className="card-actions justify-between mt-4 items-center">
-            <span className="text-sm text-base-content/60">
-                {formatDate(new Date(note.createdAt))}
-            </span>
-            <div className="flex items-center gap-1">
-                <PenSquareIcon className="size-5"/>
-                <button className="btn btn-ghost btn-xs text-error">
-                    <Trash2Icon className="size-4"/>
-                </button>
-            </div>
+          <span className="text-sm text-base-content/60">
+            {formatDate(new Date(note.createdAt))}
+          </span>
+          <div className="flex items-center gap-1">
+            <PenSquareIcon className="size-5" />
+            <button
+              className="btn btn-ghost btn-xs text-error"
+              onClick={(e) => handleDelete(e, note._id)}
+            >
+              <Trash2Icon className="size-4" />
+            </button>
+          </div>
         </div>
       </div>
     </Link>
